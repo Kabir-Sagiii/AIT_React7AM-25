2fdd600e975f5bd86fbe91e226903bd76f2cef01
Kabir <sagithedeveloper@gmail.com>
2024-12-20 07:55:18 +0530

    Added the code

---
+++ S24/src/components/profile/ProfileContainer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Profile.css";
+import { Link, Outlet } from "react-router-dom";
+function ProfileContainer() {
+  return (
+    <div className="mainProfileContainer">
+      <div className="mainContainerLeftPanel">
+        <div>
+          <Link to="details">Personal Details</Link>
+          <hr />
+        </div>
+        <div>
+          <Link to="address">Address</Link>
+          <hr />
+        </div>
+        <div>
+          <Link to="orders">Orders</Link>
+          <hr />
+        </div>
+        <div>
+          <Link to="changepassword">Change Password</Link>
+          <hr />
+        </div>
+        <div>
+          <Link to="wishlist">Wishlist</Link>
+          <hr />
+        </div>
+      </div>
+      <hr />
+      <div className="mainContainerRightPanel">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default ProfileContainer;
